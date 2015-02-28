2e47af6221fb0894e945500f4ee354c8070de4ef
tecnologiapedromercedes <tecnologiapedromercedes@gmail.com>
2015-02-28 09:41:10 +0100

    Update Knightrider.ino

--- programas/Knightrider.ino
+++ programas/Knightrider.ino
@@ -4,27 +4,28 @@ const int NbrLEDs = 6;
 const int ledPins[] = {2, 3, 4, 5, 6, 7};
 const int wait = 30;
 void setup(){
-for (int led = 0; led < NbrLEDs; led++)
-{
-pinMode(ledPins[led], OUTPUT);
+  for (int led = 0; led < NbrLEDs; led++)
+  {
+    pinMode(ledPins[led], OUTPUT);
+  }
 }
-}
+
 void loop() {
-for (int led = 0; led < NbrLEDs-1; led++)
-{
-digitalWrite(ledPins[led], HIGH);
-delay(wait);
-digitalWrite(ledPins[led + 1], HIGH);
-delay(wait);
-digitalWrite(ledPins[led], LOW);
-delay(wait*2);
-}
-for (int led = NbrLEDs; led > 0; led--) {
-digitalWrite(ledPins[led], HIGH);
-delay(wait);
-digitalWrite(ledPins[led - 1], HIGH);
-delay(wait);
-digitalWrite(ledPins[led], LOW);
-delay(wait*2);
-}
+  for (int led = 0; led < NbrLEDs-1; led++)
+  {
+    digitalWrite(ledPins[led], HIGH);
+    delay(wait);
+    digitalWrite(ledPins[led + 1], HIGH);
+    delay(wait);
+    digitalWrite(ledPins[led], LOW);
+    delay(wait*2);
+  }
+  for (int led = NbrLEDs; led > 0; led--) {
+    digitalWrite(ledPins[led], HIGH);
+    delay(wait);
+    digitalWrite(ledPins[led - 1], HIGH);
+    delay(wait);
+    digitalWrite(ledPins[led], LOW);
+    delay(wait*2);
+  }
 }
